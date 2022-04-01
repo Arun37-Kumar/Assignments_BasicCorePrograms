00b0d822e1572717c8bf8d8a943ba29c8d85b675
Arun37-Kumar <arun007624@gmail.com>
2022-04-01 20:19:12 +0530

    commit

---
+++ BasicCorePrograms/FlipCoin.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    class FlipCoin
+    {
+        public static void FlipCoinTest()
+        {
+            int noOfTime;
+            int countHead = 0;
+            int countTail = 0;
+            Console.WriteLine("Enter the number of time to flip coin : ");
+            noOfTime = Convert.ToInt32(Console.ReadLine());
+            for (int start = 0; start < noOfTime; start++)
+            {
+                Random random = new Random();
+                double flip = random.Next(0, 1);
+                if (flip < 0.5)
+                {
+                    countTail++;
+                }
+                else
+                {
+                    countHead++;
+                }
+            }
+            int headTotalCount = countHead;
+            int tailTotalCount = countTail;
+            Console.WriteLine("Total no of Head : {0} ", headTotalCount);
+            Console.WriteLine("Total no of Tail : {0}", tailTotalCount);
+
+            double headPercent = (headTotalCount / noOfTime) * 100; 
+            double tailPercent = (tailTotalCount / noOfTime) * 100;
+
+            Console.WriteLine("Head Percentage : {0}", headPercent);
+            Console.WriteLine("Tail Percentage : {0}", tailPercent);
+
+
+        }
+    }
+}
--- BasicCorePrograms/Program.cs
+++ BasicCorePrograms/Program.cs
@@ -10,6 +10,9 @@ namespace BasicCorePrograms
     {
         static void Main(string[] args)
         {
+            FlipCoin.FlipCoinTest();
+            Console.ReadLine();
+
         }
     }
 }
